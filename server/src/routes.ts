7d2b05cdbda31861c3f67ada380b40aaa58477c8
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-08-24 23:51:25 -0300

    feat: Added try catch to treat possible errors while inserting content on database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -24,37 +24,45 @@ routes.post('/classes', async (request, response) => {
 
     const trx = await db.transaction();
 
-    const insertedUsersIds = await trx('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
+    try {
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        });
+    
+        const class_id = insertedClassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to),
+            };
+        });
+    
+        await trx('class_schedule').insert(classSchedule);
+    
+        await trx.commit();
+    
+        return response.status(201).send(); //Created with success status
+    } catch(err) {
+        await trx.rollback();
 
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await trx('classes').insert({
-        subject,
-        cost,
-        user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to),
-        };
-    });
-
-    await trx('class_schedule').insert(classSchedule);
-
-    await trx.commit();
-
-    return response.send();
+        return response.status(400).json({ //Bad Request status
+            error: 'Unexpected error while creating a new class'
+        })
+    }
 });
 
 export default routes;
